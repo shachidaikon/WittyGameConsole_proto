290f5a8a6856545e36f54650a8468a31d82b5cee
shachidaikon <tkroxy0815@gmail.com>
2021-01-27 02:08:02 +0900

    initial commit

---
+++ layouts/default.vue
@@ -0,0 +1,74 @@
+<template>
+  <v-app light>
+    <v-navigation-drawer
+      v-model="drawer"
+      :mini-variant="miniVariant"
+      :clipped="clipped"
+      fixed
+      app
+    >
+      <v-list>
+        <v-list-item
+          v-for="(item, i) in items"
+          :key="i"
+          :to="item.to"
+          router
+          exact
+        >
+          <v-list-item-action>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title v-text="item.title" />
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+    <v-app-bar :clipped-left="clipped" fixed app color="rgb(249,249,249)">
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
+
+      <v-toolbar-title class="colorrize" v-text="title" />
+    </v-app-bar>
+    <v-main class="whitenize">
+      <v-container>
+        <nuxt />
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      clipped: false,
+      drawer: false,
+      fixed: false,
+      items: [
+        {
+          icon: 'mdi-apps',
+          title: 'Welcome',
+          to: '/',
+        },
+        {
+          icon: 'mdi-chart-bubble',
+          title: 'Inspire',
+          to: '/inspire',
+        },
+      ],
+      miniVariant: false,
+      title: 'Witty Game Console',
+    }
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.colorrize {
+  color: rgb(116, 166, 76);
+  font-weight: bold;
+}
+.whitenize {
+  background-color: rgb(249, 249, 249);
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,39 @@
+<template>
+  <v-row justify="center" align="center">
+    <v-col cols="12">
+      <v-carousel cycle hide-delimiters :show-arrows="false" height="auto">
+        <v-carousel-item
+          v-for="(item, i) in items"
+          :key="i"
+          :src="item.src"
+          reverse-transition="fade-transition"
+          transition="fade-transition"
+        >
+        </v-carousel-item>
+      </v-carousel>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      items: [
+        {
+          src: 'https://cdn.vuetifyjs.com/images/carousel/squirrel.jpg',
+        },
+        {
+          src: 'https://cdn.vuetifyjs.com/images/carousel/sky.jpg',
+        },
+        {
+          src: 'https://cdn.vuetifyjs.com/images/carousel/bird.jpg',
+        },
+        {
+          src: 'https://cdn.vuetifyjs.com/images/carousel/planet.jpg',
+        },
+      ],
+    }
+  },
+}
+</script>
