98afd7b37fa773c085d6ecccc74b3c6f875d8a65
shachidaikon <tkroxy0815@gmail.com>
2021-01-27 23:12:36 +0900

    Amendments

--- pages/Carousels.vue
+++ pages/Carousels.vue
@@ -1,15 +1,14 @@
 <template>
   <v-row justify="center" align="start">
     <v-col cols="12">
-      <v-carousel cycle hide-delimiters :show-arrows="false" height="250">
+      <v-carousel cycle hide-delimiters :show-arrows="false" height="350">
         <v-carousel-item
           v-for="(item, i) in items"
           :key="i"
           :src="item.src"
           reverse-transition="fade-transition"
           transition="fade-transition"
-          contain
-          max-height="250"
+          height="350"
         >
         </v-carousel-item>
       </v-carousel>
